0af5b9e5df52829c3c8bd4b6b201365c73d4b866
Brandon Lew <brandon.t.lew@gmail.com>
2024-08-05 16:04:01 -0700

    format and tighten styles

--- styles/Constants.js
+++ styles/Constants.js
@@ -4,4 +4,4 @@ export const colorBlue = '#2E4E74';
 export const colorLightGrey = '#FDFDFD';
 export const colorMediumGrey = '#F8F8F8';
 export const colorDarkGrey = '#494F56';
-export const colorRed = '#BF0000'
+export const colorRed = '#BF0000';
